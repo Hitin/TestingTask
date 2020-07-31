b23e511cbbaf9f25bf00fe9ce74971c54602e102
Petr Ablyazov <phoenix-h@mail.ru>
2020-07-31 11:57:38 +0400

    CRUD for local base

--- app/controllers/web/posts_controller.rb
+++ app/controllers/web/posts_controller.rb
@@ -1,5 +1,50 @@
 class Web::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_attrs)
+
+    if @post.save
+      redirect_to(posts_path)
+    else
+      render(action: :new)
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_attrs)
+      redirect_to(post_path(@post))
+    else
+      render(action: :edit)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to(posts_path)
+  end
+
+  private
+
+  def post_attrs
+    params.require(:post).permit(:title, :body, :userid)
   end
 end
--- test/controllers/web/posts_controller_test.rb
+++ test/controllers/web/posts_controller_test.rb
@@ -1,8 +1,54 @@
 require 'test_helper'
 
 class Web::PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = create(:post)
+  end
+
+  test 'should get index Posts' do
+    get posts_path
+    assert_response :success
+  end
+
+  test 'should get show Posts' do
+    get post_path(@post)
+    assert_response :success
+  end
+
   test 'should get new' do
     get new_post_url
     assert_response :success
+  end
+
+  test 'should post create Post' do
+    post_attrs = attributes_for(:post)
+    post posts_path, params: { post: post_attrs }
+    assert_response :redirect
+
+    post_last = Post.last
+    assert_equal post_attrs[:title], post_last.title
+  end
+
+  test 'should delete destroy Post' do
+    delete post_path(@post)
+    assert_response :redirect
+
+    assert_not Post.exists?(@post.id)
+  end
+
+  test 'should get edit Post' do
+    get edit_post_path(@post)
+    assert_response :success
+  end
+
+  test 'should put update Post' do
+    attrs = {}
+    attrs[:title] = generate(:title)
+
+    put post_path(@post), params: { post: attrs }
+    assert_response :redirect
+
+    @post.reload
+    assert_equal attrs[:title], @post.title
   end
 end
