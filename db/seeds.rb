2e7736c5c12fbe67c84c6be7d15cd3651a6afd32
Petr Ablyazov <phoenix-h@mail.ru>
2020-08-04 11:56:36 +0400

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+count = 1
+while count <= 5 do
+  Post.create(title: "Title1#{count}", body: "Body#{count}", userId: '1')
+  count += 1
+end
