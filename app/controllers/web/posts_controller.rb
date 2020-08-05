cacc007435c19f99955435e6941cccd157a71041
Petr Ablyazov <phoenix-h@mail.ru>
2020-08-05 11:59:19 +0400

    Add configurations, add migrations

--- app/controllers/web/posts_controller.rb
+++ app/controllers/web/posts_controller.rb
@@ -1,11 +1,13 @@
 class Web::PostsController < ApplicationController
+  def initialize
+    @errors = []
+  end
+
   def index
     @posts = Post.all
-    @errors = []
-    @force = params[:force]
     arr = []
-    if @force == 'true'
-      @posts.each do |post|
+    if params[:force] == 'true'
+      @posts.find_each do |post|
         arr << force_update(post)
       end
       @posts = arr
@@ -13,10 +15,8 @@ class Web::PostsController < ApplicationController
   end
 
   def show
-    @force = params[:force]
-    @errors = []
     @post = Post.find(params[:id])
-    if @force == 'true'
+    if params[:force] == 'true'
       @post = force_update(@post)
     end
   end
@@ -26,8 +26,7 @@ class Web::PostsController < ApplicationController
   end
 
   def create
-    @errors = []
-    response = change_service('http://jsonplaceholder.typicode.com/posts', post_attrs, 'Post')
+    response = ExternalService.change_service(Rails.configuration.external_api_url, post_attrs, 'Post')
     post_new = JSON.parse(response.body)
     @post = Post.new(post_new)
     if response.code == '201'
@@ -37,7 +36,7 @@ class Web::PostsController < ApplicationController
         render(action: :new)
       end
     else
-      @errors << "Id #{@post.id} - #{response.message}"
+      add_errors(@post.id, response.message)
       render(action: :new)
     end
   end
@@ -48,12 +47,11 @@ class Web::PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @errors = []
     if @post.update(post_attrs)
-      uri = "http://jsonplaceholder.typicode.com/posts/#{@post.id}"
-      response = change_service(uri, post_attrs, 'Put')
+      uri = "#{Rails.configuration.external_api_url}/#{@post.id}"
+      response = ExternalService.change_service(uri, post_attrs, 'Put')
       if response.code != '200'
-        @errors << "Id #{@post.id} - #{response.message}"
+        add_errors(@post.id, response.message)
       end
       redirect_to(post_path(@post))
     else
@@ -64,42 +62,28 @@ class Web::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    uri = "http://jsonplaceholder.typicode.com/posts/#{@post.id}"
-    service(uri, 'Delete')
+    uri = "#{Rails.configuration.external_api_url}/#{@post.id}"
+    ExternalService.service(uri, 'Delete')
     redirect_to(posts_path)
   end
 
   private
 
   def force_update(post)
-    uri = "http://jsonplaceholder.typicode.com/posts/#{post.id}"
-    response = service(uri, 'Get')
+    uri = "#{Rails.configuration.external_api_url}/#{post.id}"
+    response = ExternalService.service(uri, 'Get')
     if response.code == '200'
       post.update(JSON.parse(response.body))
     else
-      @errors << "Id #{post.id} - #{response.message}"
+      add_errors(post.id, response.message)
     end
     post
   end
 
-  def service(url, method)
-    uri = URI(url)
-    http = Net::HTTP.new(uri.host)
-    method = "Net::HTTP::#{method}".constantize
-    req = method.new(uri.path)
-    res = http.request(req)
-    res
-  end
+  
 
-  def change_service(url, attrs, method)
-    uri = URI(url)
-    headers = { 'Content-Type' => 'application/json; charset=UTF-8' }
-    http = Net::HTTP.new(uri.host)
-    method = "Net::HTTP::#{method}".constantize
-    req = method.new(uri.path, headers)
-    req.body = attrs.to_json
-    res = http.request(req)
-    res
+  def add_errors(id, message)
+    @errors << "Id #{id} - #{message}"
   end
 
   def post_attrs
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_120215) do
+ActiveRecord::Schema.define(version: 2020_08_04_174815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
+    t.string "title", null: false
+    t.string "body", null: false
     t.integer "userId"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
